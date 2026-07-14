9ff1793afe789cbfc6162eb8024f2cccfb235607
namrata <namrata.khati@yenyasoft.com>
2026-07-14 10:59:48 +0545

    initial project and route setup

---
+++ src/components/layouts/Layout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+</script>
+
+<template>
+	<Header />
+	<main class="">
+		<slot></slot>
+	</main>
+	<Footer />
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const router = createRouter({
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "Home",
+			component: () => import("../views/Home.vue"),
+		},
+	],
+	scrollBehavior(to, _from, savedPosition) {
+		if (savedPosition) {
+			return { savedPosition, behavior: "smooth" };
+		} else if (to.hash) {
+			return {
+				el: to.hash,
+				behavior: "smooth",
+			};
+		} else {
+			return {
+				top: 0,
+				behavior: "smooth",
+			};
+		}
+	},
+});
+
+export default router;
